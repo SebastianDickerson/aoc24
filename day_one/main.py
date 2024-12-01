c9b511a16707afc149f806a007c2bd11585a1e82
Seb <sebastiandickerson12@gmail.com>
2024-12-01 21:18:20 +1000

    Hit em' with a little clean up

--- day_one/main.py
+++ day_one/main.py
@@ -2,21 +2,16 @@ import sys
 import collections
 
 def partOne(data):
-    left, right = [int(line[0].rstrip()) for line in data], [int(line[3].rstrip()) for line in data]
+    left, right = [int(line[0]) for line in data], [int(line[3]) for line in data]
     return sum([abs(x - y) for x, y in zip(sorted(left), sorted(right))])
 
 def partTwo(data):
     left, right = [int(line[0].rstrip()) for line in data], [int(line[3].rstrip()) for line in data]
     right_dupes = collections.Counter(right)
-    sim_scores = []
-    for l in left:
-        sim_scores.append(l * right_dupes[l])
-        
-        
-    print("Part Two: {}".format(sum(sim_scores)))
-    
+            
+    return sum([l * right_dupes[l] for l in left])
 
 if __name__ == "__main__":
-    lines = [line.split(" ") for line in sys.stdin.readlines()]
+    lines = [line.strip().split(" ") for line in sys.stdin.readlines()]
     print("Part One: {}".format(partOne(lines)))
-    partTwo(lines)
+    print("Part Two: {}".format(partTwo(lines)))
